60fb926ae73333a117e81afa71b753ff6d8e32f0
3xclamationMark <88326849+3xclamationMark@users.noreply.github.com>
2022-01-09 09:39:36 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+//Getting the time
+setInterval(function () {
+	var time = new Date();
+	var minutes = time.getMinutes();
+	var hours = time.getHours();
+	var seconds = time.getSeconds();
+	if (seconds<10 && minutes<10){
+		document.getElementById('digital').innerHTML=hours + ':' + '0' + ':' + minutes + ':' + '0' + seconds;
+	}
+	else if(seconds<10){
+		document.getElementById('digital').innerHTML=hours + ':' + minutes+ ':' + '0' + seconds;
+	} else if (minutes<10){
+		document.getElementById('digital').innerHTML=hours + ':' + '0' + ':' + minutes + ':' + seconds;
+	} else{
+		document.getElementById('digital').innerHTML=hours + ':' + minutes+ ':' + seconds;
+	}
+
+}, 1000);
