db30545d7d7a6fae260aadd1738710d895d05f29
3xclamationMark <88326849+3xclamationMark@users.noreply.github.com>
2022-01-13 20:43:41 +0000

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ setInterval(function () {
 	var hours = time.getHours();
 	var seconds = time.getSeconds();
 	if (seconds<10 && minutes<10){
-		document.getElementById('digital').innerHTML=hours + ':' + '0' + ':' + minutes + ':' + '0' + seconds;
+		document.getElementById('digital').innerHTML=hours + ':' + '0' + minutes + ':' + '0' + seconds;
 	}
 	else if(seconds<10){
 		document.getElementById('digital').innerHTML=hours + ':' + minutes+ ':' + '0' + seconds;
